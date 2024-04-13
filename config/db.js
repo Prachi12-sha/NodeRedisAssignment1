e0c64f23be30e3412c8b310b4731028ca8cac350
Prachi12-sha <166924912+Prachi12-sha@users.noreply.github.com>
2024-04-13 20:27:05 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,13 @@
+require("dotenv").config()
+const Redis = require("ioredis")
 
+const redis = new Redis({
+    host: process.env.HOST,
+    port: process.env.REDIS_PORT
+})
+
+redis.on("error", (error) => {
+    console.error("Redis connection error:", error);
+});
+
+module.exports =redis
